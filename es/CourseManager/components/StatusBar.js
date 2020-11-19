d82d140973a6f9845d9c02226cf657821ee045b1
Chris Worfolk <chris@worfolk.co.uk>
2020-11-19 14:32:22 +0000

    Course Manager alerts styling

--- es/CourseManager/components/StatusBar.js
+++ es/CourseManager/components/StatusBar.js
@@ -13,7 +13,7 @@ const StatusBar = ({ status, clearStatus }) => {
     const message = status.message || defaultMessage(status.level);
 
     return (
-        <div className={`alert alert-${status.level} d-flex justify-content-between align-items-center`} role="alert">
+        <div className={`alert alert-${status.level} alert-flex mb-3`} role="alert">
             <div>{message}</div>
             <div>
                 <button type="button" className="close" aria-label="Close" onClick={() => clearStatus()}>
