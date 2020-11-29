09e743bbaf8fc52d2626b381e2644a271b214771
Chris Worfolk <chris@worfolk.co.uk>
2020-11-29 23:12:17 +0000

    Remove console log

--- es/lesson.js
+++ es/lesson.js
@@ -4,7 +4,6 @@ const menuButton = document.querySelector('header button');
 
 if (menuButton) {
     menuButton.onclick = function() {
-        console.log('Hello');
         document.querySelector('nav').classList.toggle('active');
     }
 }
